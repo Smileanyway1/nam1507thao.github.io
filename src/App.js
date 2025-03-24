e00860660347fb6ecf2d26130da7aa669cc99af4
nam.nguyenthanh <nam.nguyenthanh@ivnd.com.vn>
2025-03-24 21:54:07 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HeartAnimation1 from "./heart/components/heart1";
+
+function App() {
+  return (
+    <div className="App">
+      <HeartAnimation1 />
+    </div>
+  );
+}
+
+export default App;
